ea4edce1109e9bea03f9adb5b307d5aaf854283d
shin6 <rynzexel15@gmail.com>
2026-06-01 01:48:33 +0700

    add role to user migration

--- database/migrations/2026_05_31_184429_add_role_to_users_table.php
+++ database/migrations/2026_05_31_184429_add_role_to_users_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('projects', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->text('description');
-            $table->string('image');
-            $table->string('link');
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('role', ['admin', 'user'])
+                ->default('user')
+                ->after('email');
         });
     }
 
@@ -26,6 +23,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('projects');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
     }
 };
--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminUserSeeder extends Seeder
 {
@@ -12,6 +14,14 @@ class AdminUserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::updateOrCreate(
+            ['email' => 'rynzexel15@gmail.com'],
+            [
+                'name' => 'shin6',
+                'password' => Hash::make('coffelafte!'),
+                'role' => 'admin',
+                'email_verified_at' => 'now',
+            ]
+        );
     }
 }
