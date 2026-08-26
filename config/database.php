df802c933f77e04763bb031df9d8118c4e096617
shin6 <rynzexel15@gmail.com>
2026-08-27 01:04:55 +0700

    change database config to use ssl

--- config/database.php
+++ config/database.php
@@ -58,10 +58,11 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => env('DB_ENGINE', 'InnoDB'),
+            'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+                    PDO::MYSQL_ATTR_SSL_CA => true,
+                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                ]) : [],
         ],
 
         'mariadb' => [
