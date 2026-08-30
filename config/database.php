d40023c8649f7562e4c866fb1f82a163c0cf8bb6
shin6 <rynzexel15@gmail.com>
2026-08-30 21:14:44 +0700

    change db port to 4000

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '4000'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
@@ -60,7 +60,7 @@ return [
             'strict' => true,
             'engine' => env('DB_ENGINE', 'InnoDB'),
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                (defined('Pdo\Mysql::ATTR_SSL_CA') ? \Pdo\Mysql::ATTR_SSL_CA : constant('PDO::MYSQL_ATTR_SSL_CA')) => true,
             ]) : [],
         ],
 
