0d7cb817fe696120ac8b96a25e14e0c651111612
shin6 <rynzexel15@gmail.com>
2026-08-30 21:25:03 +0700

    change db port to 4000

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '4000'),
+            'port' => '4000',
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
