1a426920b9918768812cefb206c368813a88c97a
shin6 <rynzexel15@gmail.com>
2026-08-30 21:16:23 +0700

    change db port to 4000

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '4000'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
